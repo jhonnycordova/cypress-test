0f524eb30521c450eadfd8d6e644932f6142959d
Jhonny Córdova <jcordova@borealis.cl>
2018-11-02 22:49:56 -0300

    Leyendo variables a través de fixture

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,24 +1,27 @@
 'use strict'
 
 describe('Pruebas del Login', () => {
-    it('Debe cargar el login', () => {
+    
+    beforeEach( () => {
+        cy.fixture('user.json').as('userData')
         cy.visit('/login')
+        cy.contains('h1', 'Bienvenido').should('be.visible')
     })
-
-    it('Debe registrar un usuario', () => {
-        cy.visit('/login')
-        cy.contains('Crear una cuenta').click()
-        cy.get('#name').type('Usuario de Prueba')
-        cy.get('#title').type('Compañia de Prueba')
-        cy.get('#email2').type('test@test.com')
-        cy.get('#password2').type('test1234')
-        cy.contains('.button', 'Registrarse').click()
-        cy.wait(3000)
-        cy.get('.error-msg').should('not.exist')
+    
+    it.skip('Debe registrar un usuario', () => {
+        cy.get('@userData').then( (userData) => {
+            cy.contains('Crear una cuenta').click()
+            cy.get('#name').type(userData.name)
+            cy.get('#title').type(userData.company)
+            cy.get('#email2').type(userData.email)
+            cy.get('#password2').type(userData.password)
+            cy.contains('.button', 'Registrarse').click()
+            cy.wait(3000)
+            cy.get('.error-msg').should('not.exist')
+        })
     })
 
     it('Debe fallar con un usuario erróneo', () => {
-        cy.visit('/login')
         cy.get('#email1').type('noexiste@test.com')
         cy.get('#password1').type('test1234')
         cy.contains('.button', 'Ingresar').click()
@@ -27,12 +30,17 @@ describe('Pruebas del Login', () => {
     })
 
     it('Debe Loguear un usuario', () => {
-        cy.visit('/login')
-        cy.get('#email1').type('test@test.com')
-        cy.get('#password1').type('test1234')
-        cy.contains('.button', 'Ingresar').click()
-        cy.wait(3000)
-        cy.contains('a', 'Dashboard').should('be.visible')
+        cy.get('@userData').then( (userData) => {
+            cy.get('#email1').type(userData.email)
+            cy.get('#password1').type(userData.password)
+            cy.contains('.button', 'Ingresar').click()
+            cy.wait(3000)
+            cy.contains('a', 'Dashboard').should('be.visible')
+        })
+    })
+
+    after( () => {
+        cy.log('Tests Finalizados')
     })
 
 })
